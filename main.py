27743b42c66030b14f81cd42d41ba614f8b4b417
Danil <73740139+xFonzie@users.noreply.github.com>
2020-11-25 20:22:34 +0700

    another commit

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hello world')
+def main():
+    print('My first git program')
+    print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
+
+
+if __name__ == '__main__':
+    main()
